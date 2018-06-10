d9dccb3a8c8a05c5bc59af4dac30a019026f5f30
Kirill Elagin <kirelagin@gmail.com>
2018-06-10 16:38:40 +0200

    Try to remove the overrides hack

--- default.nix
+++ default.nix
@@ -5,10 +5,5 @@ let
   nixage = import (fetchTarball "https://github.com/kirelagin/nixage/archive/${nixage-commit}.tar.gz") {
     inherit system;
     config = { allowUnfree = true; };
-    overrides = self: super: {
-      base-noprelude = nixage.pkgs.haskell.lib.dontHaddock (super.base-noprelude);
-      hspec-core = nixage.pkgs.haskell.lib.dontCheck (super.hspec-core);  # fails in sandbox
-      tasty-discover = nixage.pkgs.haskell.lib.dontCheck (super.tasty-discover);  # fails
-    };
   };
 in nixage.buildYamlProject ./.
