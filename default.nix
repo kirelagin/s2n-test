6b84deeee4b5db8a60726d4c0e4b91c093ed3416
Kirill Elagin <kirelagin@gmail.com>
2018-06-11 10:09:35 +0200

    Upgrade nixage

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
 { system ? builtins.currentSystem }:
 
 let
-  nixage-commit = "df6c3d5afba513af95929e1b8e6ae3d6ab5f6656";
+  nixage-commit = "e8c5d305435c386ee6dd48666b13791432ef6028";
   nixage = import (fetchTarball "https://github.com/kirelagin/nixage/archive/${nixage-commit}.tar.gz") {
     inherit system;
   };
-in nixage.buildYamlProject ./.
+in nixage.buildYamlProject ./project.yaml
