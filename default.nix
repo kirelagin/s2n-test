937d41c2bd1bbb99cbe7cff83d7e1799d0bfa778
Kirill Elagin <kirelagin@gmail.com>
2018-06-15 02:37:19 +0300

    Try to have a dep with subdirs

--- default.nix
+++ default.nix
@@ -1,8 +1,11 @@
 { system ? builtins.currentSystem }:
 
 let
-  nixage-commit = "e8c5d305435c386ee6dd48666b13791432ef6028";
+  nixage-commit = "aa5598fec7af3ab1515b59f00714e37e1a5b7efe";
   nixage = import (fetchTarball "https://github.com/kirelagin/nixage/archive/${nixage-commit}.tar.gz") {
     inherit system;
+    overrides = self: super: {
+      base-noprelude = nixage.pkgs.haskell.lib.dontHaddock (super.base-noprelude);
+    };
   };
 in nixage.buildYamlProject ./project.yaml
